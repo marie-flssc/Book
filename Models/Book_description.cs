c817c7f59a498918d7c53d240b580ed4c4408b35
hansss117 <46796189+hansss117@users.noreply.github.com>
2021-03-25 19:38:56 +0000

    Update Book_description.cs

---
+++ Models/Book_description.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Book_Hw
+{
+    public class Book_description
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
